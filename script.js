795de66d9fcb6422b741848703ba22dfa730cf67
Ezra <ezraermy@gmail.com>
2023-02-21 23:57:35 +0300

    Fix eslint error class numbers

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint max-classes-per-file: ["error", 3] */
 // Book Class
 class Book {
   constructor(title, author) {
@@ -76,31 +77,19 @@ document.addEventListener('DOMContentLoaded', bookList.displayBooks);
 
 // Event: Add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-  // Prevent actual submit
   e.preventDefault();
-
-  // Get form values
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
 
-  // Instatiate book
   const book = new Book(title, author);
 
-  // Add Book to bookList
   bookList.addBookToList(book);
-
-  // Add book to store
   Store.addBook(book);
-
-  // Clear fields
   bookList.clearFields();
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  // Remove book from bookList
   bookList.deleteBook(e.target);
-
-  // Remove book from store
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
